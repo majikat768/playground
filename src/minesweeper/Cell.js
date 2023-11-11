14cbae6b5ba122eecbeed29a3427ee2fc6dcd069
majikat768 <mk.atkinson8@gmail.com>
2023-11-11 14:09:31 -0700

    mswpr

--- src/minesweeper/Cell.js
+++ src/minesweeper/Cell.js
@@ -1,7 +1,50 @@
-export function Cell({ x, y, onCellClick }) {
-    let mine = false;
-    return <div
+import { useState } from "react";
+
+export function Cell({ x, y, mine, neighbors, onCellClick }) {
+    const [clicked, setClicked] = useState(false);
+    const [flagged, setFlagged] = useState(false);
+    const [label, setLabel] = useState(null);
+
+    return <button
         className="cell"
-        onClick={() => onCellClick(x, y)}
-    ></div>
+        id={"c"+x+"_"+y}
+        disabled={clicked}
+        onClick={
+            (e) => {
+                click(e);
+            }
+        }
+        onContextMenu={
+            (e) => {
+                if (!clicked) {
+                    flag(e);
+                }
+            }
+        }
+    >{label}</button>
+
+    function flag(e) {
+        e.preventDefault();
+        if (flagged) {
+            setFlagged(false);
+            setLabel(null);
+        } else {
+            setFlagged(true);
+            setLabel("🚩");
+        }
+    }
+    function click(e) {
+        if (!clicked) {
+            setClicked(true);
+            if (neighbors == -1) {
+                setLabel("💣");
+            } else {
+                if (neighbors > 0) {
+                    setLabel(neighbors);
+                }
+                onCellClick(e, x, y, mine);
+            }
+        }
+
+    }
 }
--- src/minesweeper/Minesweeper.js
+++ src/minesweeper/Minesweeper.js
@@ -1,15 +1,15 @@
-import { useState } from "react";
 import "./minesweeper.css";
 import { Cell } from "./Cell";
 
 export function Minesweeper() {
 
-    const cols = 10;
-    const rows = 10;
-    const n_mines = 32;
+    const cols = 12;
+    const rows = 8;
+    const n_mines = 6;
 
-    const board = init_cells(cols, rows);
-    const mines = init_mines(cols, rows, n_mines);
+    const cells = [];
+    const mines = init_mines();
+    const board = init_cells();
 
     return <div className="minesweeper">
         <div className="board">
@@ -17,43 +17,74 @@ export function Minesweeper() {
         </div>
     </div>
 
-    function handleClick(x,y) {
-        console.log(x,y);
-        console.log(mines[x][y]);
+    function handleClick(e,x,y,mine) {
+        e.preventDefault();
+        if(get_neighbors(x,y) == 0) {
+            for(let i = -1; i <= 1; i++) {
+                for(let j = -1; j <= 1; j++) {
+                    let n_x = x+i;
+                    let n_y = y+j;
+                    if(n_x != x && n_y != y) {
+                        if(n_x >= 0 && n_x < cols && n_y >= 0 && n_y < rows) {
+                            if(!cells[n_x][n_y].clicked) {
+                            //sendClick(x,y);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    function sendClick(x,y) {
+        const events = ['mousedown','click','mouseup'];
+        events.forEach((t) => {
+            document.querySelector("#c"+x+"_"+y).dispatchEvent(
+                new MouseEvent(t, {
+                    view:window,
+                    bubbles:true,
+                    cancelable:true,
+                    buttons:1
+                })
+            )
+        })
     }
 
     function init_cells() {
         const board = [];
-        for (let i = 0; i < rows; i++) {
+        for (let i = 0; i < cols; i++) {
             let row = [];
-            for (let j = 0; j < cols; j++) {
-                row.push(
-                    <Cell
-                        key={i + "," + j}
-                        x = {i}
-                        y = {j}
-                        onCellClick={handleClick}></Cell>
-                );
+            cells.push([]);
+            for (let j = 0; j < rows; j++) {
+                let cell = <Cell
+                    key={i + "," + j}
+                    x={i}
+                    y={j}
+                    neighbors={get_neighbors(i,j)}
+                    mine={mines[i][j]}
+                    onCellClick={handleClick}></Cell>
+                cells[i].push(cell);
+                row.push(cell);
             }
             board.push(<div className="row" key={i}>{row}</div>);
         }
 
+        init_mines();
         return board;
     }
 
     function init_mines() {
         let mines = [];
-        for (let i = 0; i < rows; i++) {
+        for (let i = 0; i < cols; i++) {
             mines.push([]);
-            for (let j = 0; j < cols; j++) {
+            for (let j = 0; j < rows; j++) {
                 mines[i].push(0);
             }
         }
         let mines_planted = 0;
-
         while (mines_planted < n_mines) {
-            let x = Math.floor(Math.random() * cols);
-            let y = Math.floor(Math.random() * rows);
+            let x = Math.floor(Math.random()*cols);
+            let y = Math.floor(Math.random()*rows);
             mines[x][y] = 1;
             mines_planted += 1;
         }
@@ -61,4 +92,26 @@ export function Minesweeper() {
         return mines;
     }
 
+    function get_neighbors(x,y) {
+        if(mines[x][y] == 1) {
+            return -1;
+        }
+        let neighbors = 0;
+
+        for(let i = -1; i <= 1; i++) {
+            for(let j = -1; j <= 1; j++) {
+                let n_x = x+i;
+                let n_y = y+j;
+                if(n_x >= 0 && n_x < cols && n_y >= 0 && n_y < rows) {
+                    if(n_x != x || n_y != y) {
+                        if(mines[n_x][n_y] == 1) {
+                            neighbors += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        return neighbors;
+    }
 }
